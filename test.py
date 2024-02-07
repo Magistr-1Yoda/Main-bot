7ef4bdaf907f8a6a2c580cb7b9515dba5ad3cd9e
Magistr-1Yoda <maks.komarov.12@inbox.ru>
2024-02-07 21:57:53 +0300

    1

--- test.py
+++ test.py
@@ -3,18 +3,24 @@ db = database.Database()
 
 
 list_nub = [1,2,3,4,5,6,7,8,9,10]
-chat_id = 0
-data = db.programming(chat_id)
-number = data[1]
-idnumber = number[0]
-print(idnumber)
-if idnumber in list_nub:
-    print("Это число есть, значит я могу задать вопрос")
-    list_nub.remove(idnumber)
-    print(list_nub)
-else:
-    
-    print("Это числа нету")
+
+
+for idnumber in list_nub:
+    chat_id = 0
+    data = db.programming(chat_id)
+    number = data[1]
+    idnumber = number[0]
+    print(idnumber)
+    if idnumber in list_nub:
+        print("Это число есть, значит я могу задать вопрос")
+        list_nub.remove(idnumber)
+        print(list_nub)
+    elif idnumber not in list_nub:
+        print("Этого числа нету")
+    elif len(list_nub) == 0:
+        print("Список полностью пуст")
+        break
+
 
 
 
