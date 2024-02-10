a14589f04d5cf1081732bb86ffd446e68d34313d
Magistr-1Yoda <maks.komarov.12@inbox.ru>
2024-02-10 22:08:43 +0300

    1

--- test.py
+++ test.py
@@ -1,22 +1,21 @@
 import database
+import random
 db = database.Database()
-
 
 list_nub = [1,2,3,4,5,6,7,8,9,10]
 
-
-for idnumber in list_nub:
+for idnumber in range(30):
     chat_id = 0
-    data = db.programming(chat_id)
-    number = data[1]
-    idnumber = number[0]
-    print(idnumber)
-    if idnumber in list_nub:
+    random_number = random.choice(list_nub)
+    data = db.programming(chat_id, random_number)
+    print(random_number)
+    if random_number in list_nub:
         print("Это число есть, значит я могу задать вопрос")
-        list_nub.remove(idnumber)
+        list_nub.remove(random_number)
         print(list_nub)
-    elif idnumber not in list_nub:
+    elif random_number not in list_nub:
         print("Этого числа нет")
+        print(list_nub)
     elif len(list_nub) == 0:
         print("Список полностью пуст")
         break
