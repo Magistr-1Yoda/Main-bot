ebbd2564aada81f44dee762cdcba8290a57f42e6
Magistr-1Yoda <maks.komarov.12@inbox.ru>
2024-02-17 22:23:32 +0300

    1

--- bot2.py
+++ bot2.py
@@ -50,25 +50,27 @@ def send_question(message):
 
     list_nub = [1,2,3,4,5,6,7,8,9,10]
     random_number = random.choice(list_nub)
-    id = message.chat.id
-    chat_id = message.chat.id
-    data = db.programming(chat_id, random_number)
-    question = data[1]
-
-    keyboards = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    buttons = [types.KeyboardButton(str(i)) for i in range(1, 5)]
-    keyboards.add(*buttons)
-
-    bot.send_message(
-        chat_id,
-        f'{question[1]}\n1. {question[2]}\n2. {question[3]}\n3. {question[4]}\n4. {question[5]}'
-        ,reply_markup=keyboards
-    )
+    if random_number in list_nub:
         
-    bot.register_next_step_handler(message, check_answer, chat_id, data, id)
+        id = message.chat.id
+        chat_id = message.chat.id
+        data = db.programming(chat_id, random_number)
+        question = data[1]
+
+        keyboards = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        buttons = [types.KeyboardButton(str(i)) for i in range(1, 5)]
+        keyboards.add(*buttons)
+
+        bot.send_message(
+            chat_id,
+            f'{question[1]}\n1. {question[2]}\n2. {question[3]}\n3. {question[4]}\n4. {question[5]}'
+            ,reply_markup=keyboards
+        )
+            
+        bot.register_next_step_handler(message, check_answer, chat_id, data, id, list_nub, random_number)
         
 @bot.message_handler(func=lambda message: True)
-def check_answer(message, chat_id, data, id):
+def check_answer(message, chat_id, data, id, list_nub, random_number):
     try:
         answer = int(message.text)
         current_question = data[0]
@@ -77,13 +79,19 @@ def check_answer(message, chat_id, data, id):
         if answer == correct_option:
             bot.send_message(chat_id, 'Верно, вы получили 10 баллов за этот вопрос! Следующий вопрос:')
             send_question(message)
+            list_nub.remove(random_number)
             db.score(id)
+            print(list_nub)
         else:
             bot.send_message(chat_id, 'Неверно. Следующий вопрос:')
+            list_nub.remove(random_number)
             send_question(message)
+            print(list_nub)
     except ValueError:
         bot.send_message(chat_id, 'Пожалуйста, введите номер ответа')
         send_question(message)
+    if len(list_nub) == 0:
+        bot.send_message(chat_id, 'Вопросы кончились')
         
 
 @bot.message_handler(func = lambda m : m.text == 'Главное меню🔙')
