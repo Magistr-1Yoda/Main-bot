9aa4a8dea90fdce3c6393fa34b3ef068d508dde3
Magistr-1Yoda <maks.komarov.12@inbox.ru>
2024-03-17 11:32:20 +0300

    17.03.2024

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def keyboards_create(ListNameBTN, NumberColumns=2):
     return keyboards
 
 
-bot = telebot.TeleBot('6279309417:AAE88A0P3Pc8F-dw9BLiMYXqsj5pprTyP6w')
+bot = telebot.TeleBot('6616525038:AAF5FjJ5EqRj0hM3x6FGHJyQ70yk3cG3D3E')
 
 def welcome_message(message):
     id = message.chat.id
